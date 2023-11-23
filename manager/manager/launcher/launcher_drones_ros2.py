1e9908ee17bd85c4d2f20fdfade12d46a86f972b
pariaspe <pedro.ariasp@upm.es>
2023-11-23 15:08:00 -0600

    testing launching gazebo fortress

--- manager/manager/launcher/launcher_drones_ros2.py
+++ manager/manager/launcher/launcher_drones_ros2.py
@@ -30,17 +30,18 @@ class LauncherDronesRos2(ILauncher):
 
         # Launching MicroXRCE and Aerostack2 nodes
         as2_launch_cmd = f"ros2 launch jderobot_drones as2_default_classic_gazebo.launch.py world_file:={world_file}"
+        as2_launch_cmd = "ros2 launch ros_gz_sim gz_sim.launch.py gz_args:=empty.sdf"
 
         as2_launch_thread = DockerThread(as2_launch_cmd)
         as2_launch_thread.start()
         self.threads.append(as2_launch_thread)
 
         # Launching gzserver and PX4
-        px4_launch_cmd = f"$AS2_GZ_ASSETS_SCRIPT_PATH/default_run.sh {world_file}"
+        # px4_launch_cmd = f"$AS2_GZ_ASSETS_SCRIPT_PATH/default_run.sh {world_file}"
 
-        px4_launch_thread = DockerThread(px4_launch_cmd)
-        px4_launch_thread.start()
-        self.threads.append(px4_launch_thread)
+        # px4_launch_thread = DockerThread(px4_launch_cmd)
+        # px4_launch_thread.start()
+        # self.threads.append(px4_launch_thread)
 
         self.running = True
 
@@ -55,9 +56,12 @@ class LauncherDronesRos2(ILauncher):
             self.running = False
 
     def _set_environment(self):
-        resource_folders = [os.path.expandvars(path) for path in self.resource_folders]
-        model_folders = [os.path.expandvars(path) for path in self.model_folders]
-        plugin_folders = [os.path.expandvars(path) for path in self.plugin_folders]
+        resource_folders = [os.path.expandvars(
+            path) for path in self.resource_folders]
+        model_folders = [os.path.expandvars(path)
+                         for path in self.model_folders]
+        plugin_folders = [os.path.expandvars(
+            path) for path in self.plugin_folders]
 
         os.environ["GAZEBO_RESOURCE_PATH"] = f"{os.environ.get('GAZEBO_RESOURCE_PATH', '')}:{':'.join(resource_folders)}"
         os.environ["GAZEBO_MODEL_PATH"] = f"{os.environ.get('GAZEBO_MODEL_PATH', '')}:{':'.join(model_folders)}"
