77b78cac7b56ec956decaa9dc12fe48fce32e6de
ReyDoran <manetoscopio@gmail.com>
2023-12-26 12:51:59 +0100

    Ignored import module errors for cv2 and ompl

--- manager/manager/lint/linter.py
+++ manager/manager/lint/linter.py
@@ -70,10 +70,17 @@ class Lint:
 
             # Removes ompl E1101 error
             ompl_error = re.search(
-                r":[0-9]+: error \(E1101, no-member, .*\) Module 'ompl.*\n", result)
+                r":[0-9]+: error \(E1101, no-member, *\) Module 'ompl.*\n", result)
             if (ompl_error != None):
                 result = result[:ompl_error.start()] + \
                     result[ompl_error.end():]
+            
+            # Removes cv2 E1101 error
+            cv2_error = re.search(
+                r":[0-9]+: error \(E1101, no-member, *\) Module 'cv2.*\n", result)
+            if (cv2_error != None):
+                result = result[:cv2_error.start()] + \
+                    result[cv2_error.end():]
 
             # Removes no value for argument 'self' error
             self_exception = re.search(
