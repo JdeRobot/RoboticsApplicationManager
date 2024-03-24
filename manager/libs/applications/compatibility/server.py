ee8e479c8e2076977e9ac75146b71c1e3b62e5a9
Óscar Martínez <oscar.personal@tutanota.com>
2024-03-24 11:09:47 +0100

    Support for new superthin template for follow_person

--- manager/libs/applications/compatibility/server.py
+++ manager/libs/applications/compatibility/server.py
@@ -7,10 +7,14 @@ from src.manager.ram_logging.log_manager import LogManager
 
 
 class Server(threading.Thread):
-    def __init__(self, port, callback,):
+    def __init__(
+        self,
+        port,
+        callback,
+    ):
         super().__init__()
         self.update_callback = callback
-        self.server = WebsocketServer(port=port, host='127.0.0.1')
+        self.server = WebsocketServer(port=port, host="127.0.0.1")
         self.server.set_fn_new_client(self.on_open)
         self.server.set_fn_client_left(self.on_close)
         self.server.set_fn_message_received(self.on_message)
@@ -39,9 +43,7 @@ class Server(threading.Thread):
     def on_message(self, client, server, message):
         payload = json.loads(message)
         self.update_callback(payload)
-        server.send_message(client, "#ack")
-        LogManager.logger.debug(
-            f"Message received from template: {message[:30]}")
+        LogManager.logger.debug(f"Message received from template: {message[:30]}")
 
     def on_close(self, client, server):
         LogManager.logger.info("Connection with client closed")
