852f41e4568df8ff24ecad7e74d9791bb5326c05
Arcane-01 <bt20ece046@students.vnit.ac.in>
2025-01-25 01:01:24 +0200

    calling aerostack2 reset service if its available

--- manager/manager/manager.py
+++ manager/manager/manager.py
@@ -607,9 +607,8 @@ ideal_cycle = 20
             rosservice.call_service("/gazebo/reset_world", [])
         elif self.visualization_type == "gzsim_rae":
             self.call_gzservice("$(gz service -l | grep '^/world/\w*/control$')","gz.msgs.WorldControl","gz.msgs.Boolean","3000","reset: {all: true}")
-
-            # if self.is_ros_service_available("/drone0/platform/state_machine/_reset"):
-            #     self.call_service()
+            if self.is_ros_service_available("/drone0/platform/state_machine/_reset"):
+                self.call_service("/drone0/platform/state_machine/_reset", "std_srvs/srv/Trigger", "{}")
         else:
             self.call_service("/reset_world", "std_srvs/srv/Empty")
 
