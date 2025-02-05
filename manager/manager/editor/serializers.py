597209beda1e5606b8f5d7a960e5f406543bec0f
Javier Izquierdo Hernandez <javizqh@gmail.com>
2025-02-05 13:49:57 +0100

    Change name_with_symbols to code

--- manager/manager/editor/serializers.py
+++ manager/manager/editor/serializers.py
@@ -5,7 +5,7 @@ class Completion:
 
     def __init__(self,completion):
         self.label = completion.name
-        self.name_with_symbols = completion.name_with_symbols
+        self.code = completion.name_with_symbols
         self.docstring = completion.docstring(raw=True)
         self.type = completion.type
         if completion.parent() is None:
@@ -27,6 +27,6 @@ from rest_framework import serializers
 class CompletionSerializer(serializers.Serializer):
     label = serializers.CharField(max_length=200)
     detail = serializers.CharField(max_length=200)
-    name_with_symbols = serializers.CharField(max_length=200)
+    code = serializers.CharField(max_length=200)
     type = serializers.CharField(max_length=64)
     docstring = serializers.CharField(max_length=1000)
