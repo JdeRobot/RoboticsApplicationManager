9f3ce73635c8b55b93c17ba97c868e0d791b1db3
Javier Izquierdo Hernandez <javizqh@gmail.com>
2025-02-05 18:09:32 +0100

    Change as reset order

--- manager/manager/manager.py
+++ manager/manager/manager.py
@@ -744,9 +744,9 @@ ideal_cycle = 20
         if "noetic" in str(self.ros_version):
             rosservice.call_service("/gazebo/reset_world", [])
         elif self.visualization_type in ["gzsim_rae", "bt_studio_gz"]:
-            self.call_gzservice("$(gz service -l | grep '^/world/\w*/control$')","gz.msgs.WorldControl","gz.msgs.Boolean","3000","reset: {all: true}")
             if self.is_ros_service_available("/drone0/platform/state_machine/_reset"):
                 self.call_service("/drone0/platform/state_machine/_reset", "std_srvs/srv/Trigger", "{}")
+            self.call_gzservice("$(gz service -l | grep '^/world/\w*/control$')","gz.msgs.WorldControl","gz.msgs.Boolean","3000","reset: {all: true}")
         else:
             self.call_service("/reset_world", "std_srvs/srv/Empty")
 
