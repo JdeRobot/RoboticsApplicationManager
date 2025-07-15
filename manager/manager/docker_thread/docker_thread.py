b7938564ca427974e606fb45b27df080143f623a
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-07-15 08:21:55 +0200

    Add debugger

--- manager/manager/docker_thread/docker_thread.py
+++ manager/manager/docker_thread/docker_thread.py
@@ -4,6 +4,7 @@ import threading
 import subprocess
 import os
 import signal
+import sys
 
 
 class DockerThread(threading.Thread):
@@ -19,7 +20,7 @@ class DockerThread(threading.Thread):
         self.process = subprocess.Popen(
             self.cmd,
             shell=self.shell,
-            stdout=subprocess.PIPE,
+            stdout=sys.stdout,
             stderr=subprocess.PIPE,
             start_new_session=True,
             bufsize=1024,
