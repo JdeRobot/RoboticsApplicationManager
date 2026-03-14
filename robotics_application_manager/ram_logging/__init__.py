08a3b9a8777ed584960e7343a3a7bcfefc6fc185
PragatiGHub <guptapragati291@gmail.com>
2026-03-14 16:27:13 +0530

    Add inline documentation for core modules (comms, libs, ram_logging)

--- robotics_application_manager/ram_logging/__init__.py
+++ robotics_application_manager/ram_logging/__init__.py
@@ -1,1 +1,7 @@
+"""
+Logging utilities for Robotics Application Manager.
+
+This module initializes the logging configuration used
+throughout the RAM system.
+"""
 from .log_manager import LogManager
